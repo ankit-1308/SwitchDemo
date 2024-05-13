e3df8d6e4f3e64ee3a0de6708d6061389f6acdac
Ankit Maurya <mauryaankit1308@gmail.com>
2024-05-13 22:28:08 +0530

    succesfully implemented a switch

--- SwitchDemo/ViewController.swift
+++ SwitchDemo/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchObject: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switchObject.isOn = false
     }
 
-
+    @IBAction func actSwiych(_ sender: UISwitch) {
+        if sender.isOn {
+            printContent("Switch is on")
+            view.backgroundColor = UIColor.red
+        } else {
+            printContent("Switch is off")
+            view.backgroundColor = UIColor.yellow
+        }
+    }
+    
+    func printContent(_ message: String) {
+        // Print message to console
+        print(message)
+    }
 }
 
